2ab8ba4c24a0b592b994e402433b0c27bc053e17
VivianaS14 <vasorianp@gmail.com>
2023-05-12 17:18:06 -0500

    take input's value with FormData

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
 import { useRef } from "react";
-import "./App.css";
 import { Movies } from "./components/movies";
 import { useMovies } from "./hooks/useMovies";
+import "./App.css";
 
 function App() {
   const { movies } = useMovies();
   // useRef() te permite crear una referencia mutable que PERSISTE durante todo el ciclo de vida de tu componente (entre renderizados)
   // util para guardar cualquier valor que puedas mutar, que cada vez que cambia NO se renderiza/dispara otra vez el componente
   // util para guardar referencias del DOM
-  const inputRef = useRef();
+  //const inputRef = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const value = inputRef.current.value;
-    console.log(value);
+    const fields = new FormData(e.target);
+    const query = fields.get("query");
+    console.log(query);
   };
 
   return (
@@ -21,7 +22,7 @@ function App() {
       <header>
         <h1>Buscador de Películas</h1>
         <form className="form" onSubmit={handleSubmit}>
-          <input ref={inputRef} placeholder="Avengers, Star Wars, The Matrix" />
+          <input name="query" placeholder="Avengers, Star Wars, The Matrix" />
           <button type="submit">Buscar</button>
         </form>
       </header>
