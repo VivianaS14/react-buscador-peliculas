9807f1c1d828c2ebc86cfe351a37e2afe2c3ce50
VivianaS14 <vasorianp@gmail.com>
2023-05-12 18:40:10 -0500

    useRef for fist render input validation

--- src/hooks/useSearch.js
+++ src/hooks/useSearch.js
@@ -1,10 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export function useSearch() {
   const [search, updateSearch] = useState("");
   const [error, setError] = useState(null);
+  const isFirstInput = useRef(true);
 
   useEffect(() => {
+    if (isFirstInput.current) {
+      // si es el primer render y es vació no set el error
+      isFirstInput.current = search === "";
+      return;
+    }
+
     if (search === "") {
       setError("No se puede buscar uns película vacía");
       return;
